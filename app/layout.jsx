1c8fed6828a297694aafa5ba1b4e52b022558fc7
Ayush Paudel <[rangarokhi7@gmail.com]>
2025-12-30 12:48:56 +0545

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import { Outfit } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import StoreProvider from "@/app/StoreProvider";
+import "./globals.css";
+import {ClerkProvider} from "@clerk/nextjs";
+const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
+
+export const metadata = {
+    title: "Hamro Cart",
+    description: "Shop Smarter",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        
+        <html lang="en">
+            <body className={`${outfit.className} antialiased`}>
+                <ClerkProvider>
+                <StoreProvider>
+                    <Toaster />
+                    {children}
+                </StoreProvider>
+                </ClerkProvider>
+            </body>
+        </html>
+        
+    );
+}
