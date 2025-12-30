a16c2f9c15105dfd1f17149ddf52966f4651a723
Ayush Paudel <[rangarokhi7@gmail.com]>
2025-12-30 20:02:54 +0545

    force_dynamic added

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,16 +1,19 @@
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+import {
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/inngest/functions";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
   ],
 });
